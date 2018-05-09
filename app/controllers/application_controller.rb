9ed3e4ae4bb40710cbc59d24a1c1c9bdfa70e343
Tomohiro Omata <ajadj17@gmail.com>
2018-05-09 19:28:48 +0900

    add strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:acount_update, keys: [:first_name, :last_name])
+    #sanitizer == 清浄剤
+  end
 end
