11c3bcab6c30dca08c43cc72442c71fe616ce9de
Tomohiro Omata <ajadj17@gmail.com>
2018-05-14 10:06:49 +0900

    add registration mail

--- app/models/japanese.rb
+++ app/models/japanese.rb
@@ -2,7 +2,7 @@ class Japanese < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   validates :first_name, presence: true, length: { maximum: 10 }
   validates :last_name, presence: true, length: { maximum: 10 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
-  
-  get 'pages/home'
+
   devise_for :japaneses
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
