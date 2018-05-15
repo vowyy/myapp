a504a00d31c18b57742e0e8a7685bf7f6cecea2b
Tomohiro Omata <ajadj17@gmail.com>
2018-05-15 14:09:08 +0900

    command rails db:migrate

--- app/models/foreigner.rb
+++ app/models/foreigner.rb
@@ -4,3 +4,40 @@ class Foreigner < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
+# == Schema Information
+#
+# Table name: foreigners
+#
+#  id                     :bigint(8)        not null, primary key
+#  birthday               :date
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string(255)
+#  confirmed_at           :datetime
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string(255)
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  gender                 :integer          not null
+#  image                  :string(255)
+#  intro                  :text(65535)
+#  j_l                    :integer          not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string(255)
+#  name                   :string(255)
+#  provider               :string(255)
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  sign_in_count          :integer          default(0), not null
+#  uid                    :bigint(8)
+#  unconfirmed_email      :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_foreigners_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_foreigners_on_email                 (email) UNIQUE
+#  index_foreigners_on_reset_password_token  (reset_password_token) UNIQUE
+#
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_070712) do
+ActiveRecord::Schema.define(version: 2018_05_15_044400) do
+
+  create_table "foreigners", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.bigint "uid"
+    t.string "provider"
+    t.integer "j_l", limit: 1, null: false
+    t.integer "gender", limit: 1, null: false
+    t.text "intro"
+    t.string "image"
+    t.date "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_foreigners_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_foreigners_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_foreigners_on_reset_password_token", unique: true
+  end
 
   create_table "japaneses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
