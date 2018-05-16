0d37bb07492158aba50fa25b984a6c06954f76cd
Tomohiro Omata <ajadj17@gmail.com>
2018-05-16 14:31:27 +0900

    Add action for facebook login/signin

--- app/controllers/foreigners/omniauth_callbacks_controller.rb
+++ app/controllers/foreigners/omniauth_callbacks_controller.rb
@@ -1,30 +1,26 @@
 # frozen_string_literal: true
 
 class Foreigners::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+  def facebook
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @foreigner = Foreigner.from_omniauth(request.env["omniauth.auth"])
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+    if @foreigner.persisted?
+      if @foreigner.new_comer?
+        sign_in @foreigner
+        redirect_to edit_foreigner_registration_path @foreigner
+        flash[:notice] = "signed in successfully with facebook. Please fill in the form below before you start"
+      else
+        sign_in_and_redirect @foreigner, event: :authentication #this will throw if @user is not activated
+        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      end
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_foreingers_registration_url
+    end
+  end
 
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+  def failure
+    redirect_to root_path
+  end
 end
