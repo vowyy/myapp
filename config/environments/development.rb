fecb5f033e92e8551f692e8870a67983975ddfb1
Tomohiro Omata <ajadj17@gmail.com>
2018-05-16 09:57:19 +0900

    add mailgun settings for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,11 +64,20 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
-    :address => "smtp.gmail.com",
+    :address => "smtp.mailgun.org",
     :port => 587,
-    :domain => 'smtp.gmail.com',
-    :user_name => ENV['GMAIL'],
-    :password => ENV['GMAIL_PASSWORD'],
-    :authentication => 'login',
+    :domain => ENV['DOMAIN'],
+    :user_name => ENV['MAILGUN'],
+    :password => ENV['MAILGUN_PASSWORD'],
+    :authentication => 'plain',
   }
+  # config.action_mailer.smtp_settings = {
+  #   :enable_starttls_auto => true,
+  #   :address => "smtp.gmail.com",
+  #   :port => 587,
+  #   :domain => 'smtp.gmail.com',
+  #   :user_name => ENV['GMAIL'],
+  #   :password => ENV['GMAIL_PASSWORD'],
+  #   :authentication => 'login',
+  # }
 end
