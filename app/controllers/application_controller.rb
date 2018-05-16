bd8f1ecc1ebf471e9c92db1feb18d8cef34ae174
Tomohiro Omata <ajadj17@gmail.com>
2018-05-16 14:29:30 +0900

    Remove sanitizer from application_controller.rb and Add them in registration_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   # これがないとviewで current_japanese.full_name みたいにdecoratarのメソッドが呼び出せない。
   def current_japanese
@@ -9,11 +9,16 @@ class ApplicationController < ActionController::Base
     super
   end
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birthday, :intro, :lang_l, :image, :gender])
-    # sanitizer == 清浄剤
+  def current_foreigner
+    ActiveDecorator::Decorator.instance.decorate(super) if super.present?
+    super
   end
+
+  # protected
+  #
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birthday, :intro, :lang_l, :image, :gender])
+  #   # sanitizer == 清浄剤
+  # end
 end
--- app/controllers/japaneses/registrations_controller.rb
+++ app/controllers/japaneses/registrations_controller.rb
@@ -1,8 +1,15 @@
 class Japaneses::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   protected
+
+     def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birthday, :intro, :lang_l, :image, :gender])
+        # sanitizer == 清浄剤
+     end
 
      def update_resource(resource, params)
        resource.update_without_current_password(params)
