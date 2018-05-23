ff21777b726b6f18d7c7431445c79d4eb9c65016
Tomohiro Omata <ajadj17@gmail.com>
2018-05-23 13:20:25 +0900

    Remove foreigner_id

--- db/migrate/20180523033334_create_matches.rb
+++ db/migrate/20180523033334_create_matches.rb
@@ -2,7 +2,7 @@ class CreateMatches < ActiveRecord::Migration[5.2]
   def change
     create_table :matches do |t|
       t.references :meal, foreign_key: true
-      t.references :foreigner, foreign_key: true
+      # t.references :foreigner, foreign_key: true
       t.references :japanese, foreign_key: true
       t.boolean :ok, default: false
       t.integer :budget
