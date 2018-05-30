6172fe6e30d977840c6664403893ea75a58ee216
Tomohiro Omata <ajadj17@gmail.com>
2018-05-30 16:21:13 +0900

    Add method 'current_user'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,22 @@ module ApplicationHelper
     !japanese_signed_in? && foreigner_signed_in?
   end
 
+  def current_user
+    japanese? ? current_japanese : current_foreigner
+  end
+
   def current_user_exists?
     japanese_signed_in? || foreigner_signed_in?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:previous_url] || default)
+    session.delete(:previous_url)
+  end
+
+  private
+
+  def store_location
+    session[:previous_url] = request.original_url if request.get?
   end
 end
