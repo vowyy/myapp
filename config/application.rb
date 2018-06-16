f974c322d816a61ed4be6a024daef611d96e588b
Tomohiro Omata <ajadj17@gmail.com>
2018-06-16 17:16:20 +0900

    Add new setting in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,11 @@ module Myapp
       g.stylesheets false
       g.assets false
       g.helper false
-      g.test_framework false
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        controller_specs: false,
+        routing_specs: false
     end
   end
 end
