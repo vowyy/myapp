774d745a0edb5c8d51563d060285923afcb900a1
Tomohiro Omata <ajadj17@gmail.com>
2018-07-21 11:13:16 +0900

    Add layout

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,6 @@
 class RoomsController < ApplicationController
   def index
+    render layout: "personal_foreigner"
     @rooms = japanese? ? Room.where(japanese_id: current_japanese) : Room.where(foreigner_id: current_foreigner)
   end
 
