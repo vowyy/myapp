dbb89cbf11b59e24d159ef5f06c3328c4c848f57
Tomohiro Omata <ajadj17@gmail.com>
2018-07-21 11:12:29 +0900

    Add new action'jhome' and private method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,23 @@
 class PagesController < ApplicationController
+  before_action :you_foreigner?, only: :jhome
+  before_action :you_japanese?, only: :home
+
   def home
     @meals = Meal.order("RAND()").limit(2)
+  end
+
+  def jhome
+    @meals = Meal.order("RAND()").limit(2)
+    render layout: 'application_japanese'
+  end
+
+  private
+
+  def you_foreigner?
+    redirect_to root_path if foreigner?
+  end
+
+  def you_japanese?
+    redirect_to jhome_path if japanese?
   end
 end
