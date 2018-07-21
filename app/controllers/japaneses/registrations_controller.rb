c25f444baa64d0fdd9e6bb2535d1d5664dd0265c
Tomohiro Omata <ajadj17@gmail.com>
2018-07-21 11:09:07 +0900

    Add new and create action to modify layout file

--- app/controllers/japaneses/registrations_controller.rb
+++ app/controllers/japaneses/registrations_controller.rb
@@ -3,6 +3,35 @@ class Japaneses::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  def new
+      build_resource
+      yield resource if block_given?
+      render layout: 'application_japanese'
+    end
+
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        redirect_to jhome_path
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        redirect_to jhome_path
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      redirect_to jhome_path
+    end
+  end
+
+
   protected
 
   def configure_permitted_parameters
