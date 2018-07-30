9df43f56f137831d17418f11f519e693f6882fa0
Tomohiro Omata <ajadj17@gmail.com>
2018-07-30 14:02:21 +0900

    Change column type for time

--- db/migrate/20180521024439_create_meals.rb
+++ db/migrate/20180521024439_create_meals.rb
@@ -2,7 +2,7 @@ class CreateMeals < ActiveRecord::Migration[5.2]
   def change
     create_table :meals do |t|
       t.date :date
-      t.integer :time
+      t.string  :time
       t.integer :location
       t.integer :male, null: false, defautl: 0
       t.integer :female, null: false, defautl: 0
