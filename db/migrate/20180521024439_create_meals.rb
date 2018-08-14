b6f32b9085d76e816b6aa134c72efe5a0416a9a3
Tomohiro Omata <ajadj17@gmail.com>
2018-08-14 21:35:30 +0900

    Add skype column and location_id column

--- db/migrate/20180521024439_create_meals.rb
+++ db/migrate/20180521024439_create_meals.rb
@@ -3,11 +3,12 @@ class CreateMeals < ActiveRecord::Migration[5.2]
     create_table :meals do |t|
       t.date :date
       t.string  :time
-      t.integer :location
+      t.references :location
       t.integer :male, null: false, defautl: 0
       t.integer :female, null: false, defautl: 0
       t.text :note
       t.references :foreigner
+      t.boolean :skype
 
       t.timestamps null: false
     end
