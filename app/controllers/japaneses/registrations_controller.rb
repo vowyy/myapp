aba1be43d2206498c23c3f0ee8e3de6d93bc8938
Tomohiro Omata <ajadj17@gmail.com>
2018-08-14 21:35:06 +0900

    Delete column intro

--- app/controllers/japaneses/registrations_controller.rb
+++ app/controllers/japaneses/registrations_controller.rb
@@ -60,7 +60,7 @@ class Japaneses::RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :age, :intro, :language, :lang_l, :image, :gender])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :age, :language, :lang_l, :image, :gender])
   end
 
   def update_resource(resource, params)
