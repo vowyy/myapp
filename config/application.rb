eb2b732e0deff050394c5ab5a8d04f92963981cd
Tomohiro Omata <ajadj17@gmail.com>
2018-08-25 00:33:01 +0900

    Add config.active_record.default_timezone = :local

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # DBの登録時間帯を日本に変更
+    config.active_record.default_timezone = :local
+
     #rails g した時の挙動
     config.generators do |g|
       g.skip_routes true
