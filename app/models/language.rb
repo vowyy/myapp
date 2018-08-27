f49572cc999ae25cb9ea3173e31ba128a69169cf
Tomohiro Omata <ajadj17@gmail.com>
2018-08-27 21:29:06 +0900

    Change has_many method name

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,14 +1,14 @@
 class Language < ApplicationRecord
   has_many :japaneses
 
-  has_many :is_first_langs, class_name: "Foreigner", foreign_key: 'flng_id'
-  has_many :get_second, through: :is_first_langs, source: :slng
+  has_many :has_first_langs, class_name: "Foreigner", foreign_key: 'flng_id'
+  has_many :get_second, through: :has_first_langs, source: :slng
   # @spanish = Language.find_by(lang: "Spanish")
-  # @spanish.is_first_langs => Forignerのなかでflng_idがSpanishのレコード全て
+  # @spanish.has_first_langs => Forignerのなかでflng_idがSpanishのレコード全て
   # @spanish.get_second  => flng_idがSpanishのForeingerのslng_idすべて
 
-  has_many :is_second_langs, class_name: "Foreigner", foreign_key: 'slng_id'
-  has_many :get_first, through: :is_second_langs, source: :flng
+  has_many :has_second_langs, class_name: "Foreigner", foreign_key: 'slng_id'
+  has_many :get_first, through: :has_second_langs, source: :flng
 end
 
 # == Schema Information
