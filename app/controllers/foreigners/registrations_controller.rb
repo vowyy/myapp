abeeeb7ada41fa5b91b1d92f5ff2ba37b3fdfed8
Tomohiro Omata <ajadj17@gmail.com>
2018-08-27 20:57:51 +0900

    Add other redirect option

--- app/controllers/foreigners/registrations_controller.rb
+++ app/controllers/foreigners/registrations_controller.rb
@@ -21,7 +21,13 @@ class Foreigners::RegistrationsController < Devise::RegistrationsController
         set_flash_message :notice, flash_key
       end
       bypass_sign_in resource, scope: resource_name
-      redirect_to foreigner_path current_user, layout: 'personal_user'
+
+      if current_foreigner.profile.nil? || current_foreigner.profile.intro.blank?
+        redirect_to new_profile_path, layout: 'personal_user'
+        flash[:success] = "Let's Complete introduction to post meal."
+      else
+        redirect_to foreigner_path current_foreigner, layout: 'personal_user'
+      end
     else
       clean_up_passwords resource
       set_minimum_password_length
