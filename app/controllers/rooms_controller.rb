5d76af6c633237d5f47ed5693f80b7f86e9700f7
Tomohiro Omata <ajadj17@gmail.com>
2018-08-29 01:36:45 +0900

    Delete rooms/index and move them to meals/index

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,19 +1,6 @@
 class RoomsController < ApplicationController
   before_action :valid_users, only: [:show]
 
-  def index
-    if foreigner?
-      @foreigner_all_meals      = current_foreigner.meals
-      @foreigner_offered_meals  = current_foreigner.meals.select { |my_meal| my_meal.already_offered? && !my_meal.already_matched? }
-      @foreigner_matched_meals  = current_foreigner.meals.select { |my_meal| my_meal.already_matched? }
-    else
-      @japanese_favor_meals   = current_japanese.favors.map {|my_favor| my_favor.meal }.delete_if { |meal| meal.already_matched? }
-      @japanese_offered_meals = current_japanese.matches.where(ok: false).map { |my_offer| my_offer.meal }
-      @japanese_matched_meals = current_japanese.matches.where(ok: true).map { |my_match| my_match.meal }
-    end
-    render layout: "personal_user"
-  end
-
   def show
     @rooms = japanese? ? Room.where(japanese_id: current_japanese) : Room.where(foreigner_id: current_foreigner)
     @messages = @room.messages
