2b16aaf61cee8b416382a39ee62564c2186aa021
Tomohiro Omata <ajadj17@gmail.com>
2018-08-29 23:49:41 +0900

    Add new methods for search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,25 +1,29 @@
 class PagesController < ApplicationController
   before_action :you_foreigner?, only: :jhome
   before_action :you_japanese?, only: :home
+  before_action :new_arrival_meals
 
-  def home
-    @new_meals = Meal.limit(4).order("created_at DESC")
-  end
+  def home;end
 
   def jhome
-
-    @new_meals = Meal.limit(4).order("created_at DESC")
-    @english_meals = Meal.joins(:foreigner).where(foreigners: { flng_id: 2 }).distinct.order("RAND()").limit(4)
+    @english_meals = Meal.joins(:foreigner).where(foreigners: { flng_id: 1 }).distinct.order("RAND()").limit(4)
   end
 
   def search_meals
-    language = params[:language] unless params[:language].blank?
-    location = params[:location] unless params[:location].blank?
+    @q = Meal.ransack(params[:q])
+  end
 
-    @search_result = Meal.joins(:foreigner).where(foreigners: { flng_id: language }).where(location_id: location).distinct.order("RAND()")
+  def search_meals_result
+    @q     = Meal.search(search_params)
+    @search_meals_result = @q.result.includes(:location, :foreigner)
+    render 'search_meals'
   end
 
   private
+
+  def new_arrival_meals
+    @new_meals = Meal.limit(4).order("created_at DESC")
+  end
 
   def you_foreigner?
     redirect_to root_path if foreigner?
@@ -27,5 +31,9 @@ class PagesController < ApplicationController
 
   def you_japanese?
     redirect_to jhome_path if japanese?
+  end
+
+  def search_params
+    params.require(:q).permit!
   end
 end
