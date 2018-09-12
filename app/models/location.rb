344a90df76d7f9bf6323f8a4bb377a2296488904
Tomohiro Omata <ajadj17@gmail.com>
2018-09-12 19:02:07 +0900

    Add new method

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,10 @@
 class Location < ApplicationRecord
   belongs_to :prefecture
   has_many :meal
+
+  def pick_location(current_user, path)
+    current_user.class.name == "Foreigner" || path == "/" ? name_en : name_ja
+  end
 end
 
 # == Schema Information
@@ -8,7 +12,8 @@ end
 # Table name: locations
 #
 #  id            :bigint(8)        not null, primary key
-#  name          :string(255)
+#  name_en       :string(255)
+#  name_ja       :string(255)
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #  prefecture_id :bigint(8)
