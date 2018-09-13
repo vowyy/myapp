397ff84c89b3e60442ab695f25b2c936c173738a
Tomohiro Omata <ajadj17@gmail.com>
2018-09-14 07:36:17 +0900

    Add new column for nation table

--- app/models/nation.rb
+++ app/models/nation.rb
@@ -10,7 +10,8 @@ end
 #
 #  id         :bigint(8)        not null, primary key
 #  flag       :string(255)
-#  name       :string(255)
+#  name_en    :string(255)
+#  name_ja    :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
--- db/schema.rb
+++ db/schema.rb
@@ -145,7 +145,8 @@ ActiveRecord::Schema.define(version: 2018_08_26_011449) do
   end
 
   create_table "nations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name_en"
+    t.string "name_ja"
     t.string "flag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
