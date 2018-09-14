7ca264ec99942d2698da38ce70b56491445a2e57
Tomohiro Omata <ajadj17@gmail.com>
2018-09-14 20:32:45 +0900

    Add new methods 'authenticate!' 'set_locale'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   before_action :get_meal_instance
   before_action :set_locale
+  before_action :authenticate!
 
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -19,25 +20,26 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    if japanese?
-      I18n.locale = I18n.default_locale
-    elsif foreigner?
-      I18n.locale = :en
+    if current_user_exists?
+      japanese? ? I18n.locale = I18n.default_locale : I18n.locale = :en
+    else
+      I18n.locale = params[:locale] || I18n.default_locale
     end
   end
-
-  # def default_url_options
-  #   if I18n.locale.to_sym == I18n.default_locale
-  #     super()
-  #   else
-  #     { locale: locale }
-  #   end
-  # end
 
   protected
 
   def get_meal_instance
     @meal = Meal.new
+  end
+
+  def authenticate!
+    if japanese_signed_in?
+      @current_user = current_japanese
+      true
+    else
+      authenticate_foreigner!
+    end
   end
 
   # protected
